16efbc9494cd86dc7c1f762f5a6f51e0347b5e00
Arnav!! <arnavpandita.bt24cseds@pec.edu.in>
2026-03-22 17:16:21 +0530

    Add SQL query to count distinct active users

---
+++ 02_Filtering/lc_1141.sql
@@ -0,0 +1,7 @@
+--If I want counts of each only once, then simply use DISTINCT, instead of subquery
+
+select activity_date as day, count(distinct user_id) as active_users
+from Activity
+where activity_date >= '2019-07-27'::date -interval '29 days'
+      and activity_date<='2019-07-27'
+group by activity_date;
