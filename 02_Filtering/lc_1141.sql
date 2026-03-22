dd170320db55910e1db069c58f31ab215d7088d2
Arnav!! <arnavpandita.bt24cseds@pec.edu.in>
2026-03-22 17:18:16 +0530

    Update lc_1141.sql

--- 02_Filtering/lc_1141.sql
+++ 02_Filtering/lc_1141.sql
@@ -2,6 +2,6 @@
 
 select activity_date as day, count(distinct user_id) as active_users
 from Activity
-where activity_date >= '2019-07-27'::date -interval '29 days'
+where activity_date >= '2019-07-27'::date -interval '29 days'  --In interval, that date is also included so 29 days,
       and activity_date<='2019-07-27'
 group by activity_date;
